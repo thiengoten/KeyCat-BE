cabdcb31ef23c1cf353f3a318a6d431b68c1efca
thiengoten <thiengoten6@gmail.com>
2023-03-21 02:59:46 +0700

    test: 💍 testing register fuctional

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  //TODO: Test laij controller này sau khi đã có hàm mã hóa mật khẩu
   @Post('register')
   async register(@Body() createUserDto: CreateUserDTO) {
     return this.authService.register(createUserDto);
